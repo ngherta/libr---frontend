79b5cccb238162b08fea535b3bb7dd7d155f387b
gogo6ar <gherta.nicolai@gmail.com>
2021-05-17 20:29:58 +0300

    Add login and register form

---
+++ src/app/_models/user.ts
@@ -0,0 +1,13 @@
+﻿export class User {
+
+    id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    age: number;
+    password: string;
+    role: string;
+    phone: string;
+    token: string;
+    
+}
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.less']
+})
+export class BooksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
