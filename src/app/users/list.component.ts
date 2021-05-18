e4e9ec7ad16aa8236945ef022408f46fa7a22176
gogo6ar <gherta.nicolai@gmail.com>
2021-05-18 18:19:48 +0300

    modificate angular data table (view)

--- src/app/users/list.component.ts
+++ src/app/users/list.component.ts
@@ -1,27 +1,42 @@
-﻿import { Component, OnInit } from '@angular/core';
-import { first } from 'rxjs/operators';
+﻿import {Component, OnInit} from '@angular/core';
+import {first} from 'rxjs/operators';
 
-import { AccountService } from '@app/_services';
+import {AccountService} from '@app/_services';
+import {Subject} from "rxjs";
+import {User} from "@app/_models";
 
-@Component({ templateUrl: 'list.component.html' })
+@Component({templateUrl: 'list.component.html'})
 export class ListComponent implements OnInit {
-    users = null;
+  dtOptions: DataTables.Settings = {};
+  users: User[] = [];
 
-    constructor(private accountService: AccountService) {}
+  // We use this trigger because fetching the list of persons can be quite long,
+  // thus we ensure the data is fetched before rendering
+  dtTrigger: Subject<any> = new Subject<any>();
 
-    ngOnInit() {
-        this.accountService.getAll()
-            .pipe(first())
-            .subscribe(users => this.users = users);
-    }
+  constructor(private accountService: AccountService) {
+  }
 
-    deleteUser(id: string) {
-        const user = this.users.find(x => x.id === id);
-        user.isDeleting = true;
-        this.accountService.delete(id)
-            .pipe(first())
-            .subscribe(() => {
-                this.users = this.users.filter(x => x.id !== id) 
-            });
-    }
+  ngOnInit() {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 2
+    };
+    this.accountService.getAll()
+      .pipe(first())
+      .subscribe(users => {
+        this.users = users;
+        this.dtTrigger.next();
+      });
+  }
+
+  deleteUser(id: string) {
+    const user = this.users.find(x => x.id === id);
+    // user.isDeleting = true;
+    this.accountService.delete(id)
+      .pipe(first())
+      .subscribe(() => {
+        this.users = this.users.filter(x => x.id !== id)
+      });
+  }
 }
