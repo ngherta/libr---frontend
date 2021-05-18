a953bd2bc293892a7b0213f4cfcbcf6fdd07a6d5
gogo6ar <gherta.nicolai@gmail.com>
2021-05-18 16:42:40 +0300

    add view of users for Angular

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -47,7 +47,7 @@ export class AccountService {
     }
 
     getAll() {
-        return this.http.get<User[]>(`${environment.apiUrl}/users`);
+        return this.http.get<User[]>(`${environment.apiUrl}/users/all`);
     }
 
     getById(id: string) {
