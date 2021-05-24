bc6488cd5800820a373d6143b7c92035bc522d41
Kirill <grosukirill009@gmail.com>
2021-05-24 15:24:42 +0300

    Added book save

--- src/app/_services/book.service.ts
+++ src/app/_services/book.service.ts
@@ -1,5 +1,4 @@
-﻿﻿
-import {Injectable} from '@angular/core';
+﻿﻿import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import {BehaviorSubject, Observable} from 'rxjs';
