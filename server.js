90ca0a0bd8a6c61d37974e088b9783fbd95c785e
Kirill <grosukirill009@gmail.com>
2021-06-02 17:42:24 +0300

    changed server.js port

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/libr'));
 
-app.get('/*', function(req,res) {
+app.get('/*', function (req, res) {
 
-  res.sendFile('index.html', {root:'dist/libr'});
+  res.sendFile('index.html', { root: 'dist/libr' });
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4100);
